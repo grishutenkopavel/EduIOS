963f2b80f95541e022c84780af20ae49157421c1
Pavel Grishutenko <grishutenkopp@gmail.com>
2021-09-02 20:28:37 +0300

    add menuView

--- DropDownMenu/DropDownMenu/ViewController.swift
+++ DropDownMenu/DropDownMenu/ViewController.swift
@@ -8,12 +8,30 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var dropDownMenuView: UIView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.dropDownMenuView = UIView(frame: CGRect(x: self.view.frame.minX, y: self.view.frame.maxY - 100,
+                                                     width: self.view.frame.maxX, height: 100))
+        dropDownMenuView.backgroundColor = .cyan
+        self.view.addSubview(dropDownMenuView)
+        let swipeDropDownMenuGestureRecognizer = UISwipeGestureRecognizer(target: self, action: #selector(swipeDropDownMenuGestureHandler(sender:)))
+        swipeDropDownMenuGestureRecognizer.direction = .up
+        dropDownMenuView.addGestureRecognizer(swipeDropDownMenuGestureRecognizer)
     }
 
-
+    @objc func swipeDropDownMenuGestureHandler(sender: UIGestureRecognizer) {
+        //if sender.state == .began {
+            print(sender.location(in: self.view))
+        //}
+        if sender.state == .ended {
+            
+            self.dropDownMenuView.frame = CGRect(x: self.view.frame.minX, y: self.view.frame.maxY - 400,
+                                                 width: self.view.frame.maxX, height: 400)
+//            (forAlignmentRect: CGRect(x: self.view.frame.minX,
+//                                                                 y: self.view.frame.maxY - 300,
+//                                                                 width: self.view.frame.maxX, height: 300))
+        }
+    }
 }
 
