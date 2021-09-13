c4ea397774e86696502576f4af8c3b2f8e29ef9e
Pavel Grishutenko <grishutenkopp@gmail.com>
2021-09-13 22:08:10 +0300

    add chapter 3

--- Metal/Hello, Triangle!/Hello, Triangle!/ViewController.swift
+++ Metal/Hello, Triangle!/Hello, Triangle!/ViewController.swift
@@ -8,18 +8,82 @@ import Metal
 import QuartzCore
 import UIKit
 
+let vertexData:[Float] = [
+    0.0, 0.5, 0.0,
+    -0.5, -0.5, 0.0,
+    0.5, -0.5, 0.0]
+
 class ViewController: UIViewController {
 
     var device: MTLDevice! = nil
-    var metalLayer: CAMetalLayer
+    var metalLayer: CAMetalLayer!
+    var vertexBuffer: MTLBuffer! = nil
+    var pipelineState: MTLRenderPipelineState! = nil
+    var commandQueue: MTLCommandQueue! = nil
+    var timer: CADisplayLink! = nil
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         device = MTLCreateSystemDefaultDevice()
         
+        metalLayer = CAMetalLayer()
+        metalLayer.device = device
+        metalLayer.pixelFormat = .bgra8Unorm
+        metalLayer.framebufferOnly = true
+        metalLayer.frame = self.view.layer.frame
+        self.view.layer.addSublayer(metalLayer)
         
+        let dataSize = vertexData.count *
+            MemoryLayout.size(ofValue: vertexData[0])
+        vertexBuffer = device.makeBuffer(bytes: vertexData,
+                                        length: dataSize,
+                                       options: .storageModeShared)
+        
+        let defaultLibrary = device.makeDefaultLibrary()
+        let fragmentProgram = defaultLibrary!.makeFunction(name: "basic_fragment")
+        let vertexProgram = defaultLibrary!.makeFunction(name: "basic_vertex")
+        
+        let pipelineStateDescriptor = MTLRenderPipelineDescriptor()
+        pipelineStateDescriptor.vertexFunction = vertexProgram
+        pipelineStateDescriptor.fragmentFunction = fragmentProgram
+        pipelineStateDescriptor.colorAttachments[0].pixelFormat = .bgra8Unorm
+        do {
+        try pipelineState = device.makeRenderPipelineState(descriptor:
+            pipelineStateDescriptor)
+        } catch let error {
+            print("Failed to create pipeline state, error \(error)")
+        }
+        
+        commandQueue = device.makeCommandQueue()
+        
+        timer = CADisplayLink(target: self, selector: #selector(ViewController.gameloop))
+        timer.add(to: RunLoop.main, forMode: RunLoop.Mode.default)
+
     }
-
-
+    func render() {
+        let renderPassDescriptor = MTLRenderPassDescriptor()
+        guard let drawable = metalLayer.nextDrawable() else {return}
+        renderPassDescriptor.colorAttachments[0].texture = drawable.texture
+        renderPassDescriptor.colorAttachments[0].loadAction = .clear
+        renderPassDescriptor.colorAttachments[0].clearColor =
+            MTLClearColor(red: 221.0/255.0, green: 160.0/255.0,
+            blue: 221.0/255.0, alpha: 1.0)
+        
+        let commandBuffer = commandQueue.makeCommandBuffer()
+        
+        let renderEncoder = commandBuffer!.makeRenderCommandEncoder(descriptor: renderPassDescriptor)
+        renderEncoder!.setRenderPipelineState(pipelineState)
+        renderEncoder!.setVertexBuffer(vertexBuffer, offset: 0, index: 0)
+        renderEncoder!.drawPrimitives(type: .triangle, vertexStart: 0, vertexCount: 3)
+        renderEncoder!.endEncoding()
+        commandBuffer!.present(drawable)
+        commandBuffer!.commit()
+    }
+    
+    @objc func gameloop() {
+        autoreleasepool {
+            self.render()
+    } }
 }
 
