6d2ab500cc6bed7edff6dc970480182ae7017755
Pavel Grishutenko <grishutenkopp@gmail.com>
2021-10-09 23:08:03 +0300

    add table view

--- Fonts/Fonts/ViewController.swift
+++ Fonts/Fonts/ViewController.swift
@@ -12,21 +12,27 @@ class ViewController: UIViewController, UITableViewDataSource {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        tableView = UITableView(frame: CGRect(x: 0, y: 0,
+        tableView = UITableView(frame: CGRect(x: 0, y: 20,
                                               width: self.view.frame.maxX, height: self.view.frame.maxY) )
-        tableView?.
+        tableView?.dataSource = self
+        self.view.addSubview(tableView!)
     }
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        
+        print("namber of rows in section \(section)")
+        return 100;
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
+        print("cell for row at index path: {\(indexPath.section), \(indexPath.row)}")
+        let cell = UITableViewCell(style: .default, reuseIdentifier: "c1")
+        cell.textLabel?.text = "Section = \(indexPath.section), row = \(indexPath.row)}"
+        return cell
     }
     
     func numberOfSections(in tableView: UITableView) -> Int {
-        print("namber of ")
+        print("namber of section in tableView")
+        return 1
     }
 }
 
